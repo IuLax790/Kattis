d0174486b74b872f9ab17bec041ccf83b9edc9b2
Ido Koubi <66007024+IuLax790@users.noreply.github.com>
2024-08-31 22:25:23 +0300

    Create monopoly.py

---
+++ monopoly.py
@@ -0,0 +1,10 @@
+probs = [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1]
+
+hotels = int(input())
+spaces = set([int(i) for i in input().split()])
+
+chance = 0
+for i in spaces:
+    chance += probs[i-2]
+
+print(chance/36)
