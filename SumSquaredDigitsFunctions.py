bc9e2ca3a1c4834874df86ae42d4df43bcd47314
Ido Koubi <66007024+IuLax790@users.noreply.github.com>
2024-09-19 22:33:25 +0300

    Create SumSquaredDigitsFunctions.py

---
+++ SumSquaredDigitsFunctions.py
@@ -0,0 +1,9 @@
+numberOfTests = int(input())
+for _ in range(numberOfTests):
+    testNumber, base, number = list(map(int, input().split()))
+    result = 0
+    while 0 < number:
+        cipher = number % base
+        number //= base
+        result += cipher ** 2
+    print("{} {}".format(testNumber, result))
