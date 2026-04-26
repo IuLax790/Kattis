d0486e12e6ecb3b26cc449cf647f9d52dbb35b3d
Ido Koubi <66007024+IuLax790@users.noreply.github.com>
2026-04-26 16:11:25 +0300

    Create HydroPotential.py

---
+++ HydroPotential.py
@@ -0,0 +1,15 @@
+g = 10
+
+m1 = int(input())
+h1 = int(input())
+m2 = int(input())
+h2 = int(input())
+m3 = int(input())
+h3 = int(input())
+
+e1 = m1*g*h1
+e2 = m2*g*h2
+e3 =m3*g*h3
+print(e1)
+print(e2)
+print(e3)
