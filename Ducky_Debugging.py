38c33f35a28d31e9fc21d501f762704731522173
Ido Koubi <66007024+IuLax790@users.noreply.github.com>
2026-08-11 13:21:55 +0300

    Create Ducky_Debugging.py

---
+++ Ducky_Debugging.py
@@ -0,0 +1,11 @@
+s = input()
+while s!="I quacked the code!":
+    if s[-1]=="?":
+        print("Quack!")
+        s = input()
+
+    if s[-1]==".":
+        print("*Nod*")
+        s = input()
+
+print("")
